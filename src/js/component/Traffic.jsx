ac9d78f899495373eb113a3b642e60b624c01dc3
Annie Góngora <160527392+AnnieyG@users.noreply.github.com>
2025-01-24 16:10:29 +0000

    First Commit

--- src/js/component/Traffic.jsx
+++ src/js/component/Traffic.jsx
@@ -1,5 +1,4 @@
 import React, {useState}  from "react";
-
 <link ref= "stylesheet" href="styles.css"/>
 //creat your first component*
 export const Trafficligth = () => {
@@ -7,12 +6,13 @@ export const Trafficligth = () => {
    return (
      <div classNAme="container mt-7">
         <div className="container d-table traffic-light w-auto rounded-4 left">
-           <div onClickCapture={() => setSelectedColor("red")} className={"light red glow" + (selectedColor === "red" ? "glow" : "")}></div>
-           <div onClickCapture={() => setSelectedColor("yellow")} className={"light yellow glow" + (selectedColor == "yellow" ? "glow" : "")}></div>
+           <div onClickCapture={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? "glow" : "")}></div>
+           <div onClickCapture={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor == "yellow" ? "glow" : "")}></div>
            <div onClickCapture={() => setSelectedColor("green")} className={"light green glow" + (selectedColor === "green" ? "glow" : "")}></div>
        </div>
-              <div className="pole"></div>
-            </div>
+         <div className= "pole"></div>
+        </div>
+
     );
 
 };

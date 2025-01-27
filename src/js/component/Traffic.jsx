c4d359ee09320a6edf1150c078a0fa076bf5e45b
Annie Góngora <160527392+AnnieyG@users.noreply.github.com>
2025-01-27 09:42:00 +0000

    First Commit

--- src/js/component/Traffic.jsx
+++ src/js/component/Traffic.jsx
@@ -1,19 +1,22 @@
-import React, {useState}  from "react";
-<link ref= "stylesheet" href="styles.css"/>
+import { useState } from "react";
+import React, { useState } from "react";
+
+
+<link ref="stylesheet" href="styles.css" />
 //creat your first component*
 export const Trafficligth = () => {
-   const [selectedColor, setSelectedColor] = useState ("orange");
-   return (
-     <div classNAme="container mt-7">
-        <div className="container d-table traffic-light w-auto rounded-4 left">
-           <div onClickCapture={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? "glow" : "")}></div>
-           <div onClickCapture={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor == "yellow" ? "glow" : "")}></div>
-           <div onClickCapture={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? "glow" : "")}></div>
-       </div>
-         <div className= "pole"></div>
-        </div>
+  const [selectedColor, setSelectedColor] = useState("yellow");
+  return (
+    <div classNAme="container mt-7">
+      <div className="container d-table traffic-light w-auto rounded-4 left">
+        <div onClickCapture={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? "" : "glow")}></div>
+        <div onClickCapture={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor == "yellow" ? "" : "glow2")}></div>
+        <div onClickCapture={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? "" : "glow1")}></div>
+      </div>
+      <div className="pole"></div>
+    </div>
 
-    );
+  );
 
 };
-export default Trafficligth;
+export default Home;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,17 @@
 import React, {useState}  from "react";
 
+
 <link ref= "stylesheet" href="styles.css"/>
 //creat your first component*
 export const Trafficligth = () => {
    const [selectedColor, setSelectedColor] = useState ("orange");
+   
    return (
      <div classNAme="container mt-7">
         <div className="container d-table traffic-light w-auto rounded-4 left">
-           <div onClickCapture={() => setSelectedColor("red")} className={"light red glow" + (selectedColor === "red" ? "glow" : "")}></div>
-           <div onClickCapture={() => setSelectedColor("yellow")} className={"light yellow glow" + (selectedColor == "yellow" ? "glow" : "")}></div>
-           <div onClickCapture={() => setSelectedColor("green")} className={"light green glow" + (selectedColor === "green" ? "glow" : "")}></div>
+           <div onClickCapture={() => setSelectedColor("red")} className={"light red glow" + (selectedColor === "red" ? "" : "glow2")}></div>
+           <div onClickCapture={() => setSelectedColor("yellow")} className={"light yellow glow" + (selectedColor === "yellow" ? "" : "glow")}></div>
+           <div onClickCapture={() => setSelectedColor("green")} className={"light green glow" + (selectedColor === "green" ? "" : "glow")}></div>
        </div>
               <div className="pole"></div>
             </div>
